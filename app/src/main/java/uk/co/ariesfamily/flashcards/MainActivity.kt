81db497d0e32bb863e396e4d8bc73795663430c6
Robert3141 <robertaries1@gmail.com>
2018-11-17 08:06:57 +0000

    themes now work!
    The theme can be changed by the theme button

--- app/src/main/java/uk/co/ariesfamily/flashcards/MainActivity.kt
+++ app/src/main/java/uk/co/ariesfamily/flashcards/MainActivity.kt
@@ -6,6 +6,8 @@ import android.content.*
 import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.*;
+import android.preference.PreferenceManager
+import android.provider.Settings
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.widget.Toast
@@ -19,7 +21,9 @@ class MainActivity : AppCompatActivity() {
     //create public
 
     //create privates
-    private val FILEREQUESTCODE = 50
+    private val wordsFileRequestCode = 50
+    private val themeFileRequestCode = 51
+    private val recentFileRequestCode = 52
     private var wordsNotDefinition = true
     private var cardSelected = 0
     private var wordsFileArrayCounter = 0
@@ -27,12 +31,18 @@ class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.ActivityTheme_Primary_Base_Dark)
+        //get saved theme
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val themeName = pref.getString("Theme1","Default")
+        if (themeName == "Dark"){
+            setTheme(R.style.ActivityTheme_Primary_Base_Dark)
+        } else {
+            setTheme(R.style.AppTheme)
+        }
+
+        //create interface
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //theme switching
-
 
         //disable buttons
         flipFlashcard.isClickable = false
@@ -42,7 +52,7 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == FILEREQUESTCODE && resultCode == RESULT_OK) {
+        if (requestCode == wordsFileRequestCode && resultCode == RESULT_OK) {
             val fileSelectedPath = data?.data
             if (fileSelectedPath != null) {
                 val inputStream = contentResolver.openInputStream(fileSelectedPath)
@@ -56,12 +66,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun clickThemeChanger(view:android.view.View){
+        //check theme
+        var pref = PreferenceManager.getDefaultSharedPreferences(this)
+        var themeName = pref.getString("Theme1","Default")
+        var editor = pref.edit()
 
+        if(themeName == "Dark") {
+            editor.putString("Theme1","Default")
+        } else {
+            editor.putString("Theme1","Dark")
+        }
+        //push theme
+        editor.commit()
+
+        //reload activity
+        recreate()
     }
 
     fun clickFlipFlashcard(view: android.view.View){
         //read words file
-        var wordsFileArray = readWordsFile()
+        val wordsFileArray = readWordsFile()
 
         if(flipFlashcard.isClickable) {
             if(wordsNotDefinition) {
@@ -82,7 +106,7 @@ class MainActivity : AppCompatActivity() {
 
 
         //read file and put to array
-        var wordsFileArray = readWordsFile()
+        val wordsFileArray = readWordsFile()
 
         //randomly select number and set relevant variables
         if (wordsFileArrayCounter > 0) {
@@ -105,26 +129,23 @@ class MainActivity : AppCompatActivity() {
         newFlashcard.isClickable = true
 
         //request file
-        startActivityForResult(Intent.createChooser(intent, "Select a file"), FILEREQUESTCODE)
+        startActivityForResult(Intent.createChooser(intent, "Select a file"), wordsFileRequestCode)
     }
 
     private fun readWordsFile(): kotlin.Array<String>{
         //create local variables
-        val STRINGSPLIT = '&'
-
+        val stringSplit = '&'
+        val wordsFileArray = Array(textFileString.length, {""})
         var tempString = ""
 
         //reset variables
         wordsFileArrayCounter = 0
 
-        //Temp stuff
-        //textFileString = "Card1a&Card1b&Card2a&Card2b&Card3a&Card3b&Card4a&Card4b&Card5a&Card5b&"
 
-        var wordsFileArray = Array(textFileString.length, {""})
 
         //string splitting
         for(i in textFileString.indices) {
-            if(textFileString[i].equals(STRINGSPLIT)){
+            if(textFileString[i] == stringSplit){
 
                 wordsFileArray[wordsFileArrayCounter] = tempString
                 wordsFileArrayCounter++
@@ -143,7 +164,9 @@ class MainActivity : AppCompatActivity() {
         return (min..max).random()
     }
 
-    private fun IntRange.random() =
-            ThreadLocalRandom.current().nextInt((endInclusive + 1) - start) + start
+    private fun IntRange.random() = ThreadLocalRandom.current().nextInt((endInclusive + 1) - start) + start
 
+    /*private fun getSettings(): kotlin.Array<String>{
+        return
+    }*/
 }
