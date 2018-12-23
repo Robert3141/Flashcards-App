f99aa5559eef1009c2623a1291dbd531f8cf329f
Robert3141 <robertaries1@gmail.com>
2018-12-23 17:00:10 +0000

    code added

--- app/src/main/java/uk/co/ariesfamily/flashcards/CreateCards.kt
+++ app/src/main/java/uk/co/ariesfamily/flashcards/CreateCards.kt
@@ -2,20 +2,43 @@ package uk.co.ariesfamily.flashcards
 
 
 import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_create_cards.*
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.FileOutputStream
+import java.net.URI
 
 
 class CreateCards : AppCompatActivity() {
 
     private val savedTheme1 = "Theme1"
+    private val savedWordsFile = "wordsFile"
+    private val savedPageNumber = "pageNumber"
+    private val savedFilePath = "filepath"
+    private val newwordsFileRequestCode = 100
+
+    private var textFileString = ""
+    private var wordsFileArrayCounter = 0
+    private var wordsFileArray = Array(textFileString.length, {""})
+    private var fileSelectedPath = Uri.EMPTY
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         //create locals
         val pref = PreferenceManager.getDefaultSharedPreferences(this)
         val themeName = pref.getInt(savedTheme1,0)
+        val textFileStringSaved = pref.getString(savedWordsFile,"")
+        val pageNumberSaved = pref.getInt(savedPageNumber,0)
+        val path = pref.getString(savedFilePath,"")
 
         //get saved setup
         when (themeName){
@@ -27,6 +50,55 @@ class CreateCards : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_cards)
+
+        //disable buttons
+        buttonNext.isClickable = false
+        buttonPrevious.isClickable = false
+
+        //set text file
+        /*if (textFileStringSaved != ""){
+            //set file
+            textFileString = textFileStringSaved
+            //fileSelectedPath = Uri.parse(path)
+
+            //new flashcard
+            choosePage(pageNumberSaved)
+        } else {
+
+        }*/
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        //word file found
+        if (requestCode == newwordsFileRequestCode && resultCode == RESULT_OK) {
+            fileSelectedPath = data?.data
+            if (fileSelectedPath != null) {
+                //locals
+                val inputStream = contentResolver.openInputStream(fileSelectedPath)
+                val pref = PreferenceManager.getDefaultSharedPreferences(this)
+                var editor = pref.edit()
+                editor.putString(savedFilePath,fileSelectedPath.toString())
+
+                //set file
+                textFileString = inputStream.bufferedReader().use { it.readText() }
+
+                //enable button
+                buttonNext.isClickable = true
+
+                //save text file
+                editor.putString(savedWordsFile,textFileString)
+                editor.commit()
+
+                //run choose page
+                choosePage(1)
+
+            } else {
+                //output no file exists
+                Toast.makeText(this,"No Data", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     //onclick events for launching activities
@@ -51,4 +123,112 @@ class CreateCards : AppCompatActivity() {
         val intent = Intent(this, MainActivity::class.java).apply {}
         startActivity(intent)
     }
+
+
+    fun clickSelectFileButton(view: android.view.View){
+        //create locals
+        val intent = Intent().setType("*/*").setAction(Intent.ACTION_GET_CONTENT)
+
+        //request file
+        startActivityForResult(Intent.createChooser(intent, "Select a file"), newwordsFileRequestCode)
+    }
+
+    fun clickPrevious(view: android.view.View){
+        var pref = PreferenceManager.getDefaultSharedPreferences(this)
+        var pageNo = pref.getInt(savedPageNumber,2)
+        choosePage(pageNo-1)
+    }
+
+    fun clickNext(view: android.view.View){
+        var pref = PreferenceManager.getDefaultSharedPreferences(this)
+        var pageNo = pref.getInt(savedPageNumber,1)
+        choosePage(pageNo+1)
+    }
+
+    fun choosePage(pageNo: Int){
+        //create local variables
+        var pref = PreferenceManager.getDefaultSharedPreferences(this)
+        var editor = pref.edit()
+
+        //save previous setup
+        //saveFile()
+
+        //read file and put to array
+        val wordsFileArray = readWordsFile()
+
+        //display flashcards
+        if(wordsFileArrayCounter >= pageNo * 2) {
+            editTextTerm.setText(wordsFileArray[((pageNo+1)*2-1)])
+            editTextDef.setText(wordsFileArray[((pageNo+1)*2)])
+        } else {
+            textFileString += "&&"
+            readWordsFile()
+            editTextTerm.setText(wordsFileArray[((pageNo+1)*2-1)])
+            editTextDef.setText(wordsFileArray[((pageNo+1)*2)])
+        }
+
+        //button click ability
+        buttonNext.isClickable = true
+        buttonPrevious.isClickable = pageNo > 1
+
+        //display other outputs
+        textViewNumber.text = pageNo.toString()
+        textViewFileData.text = textFileString
+
+        //save page number
+        editor.putInt(savedPageNumber,pageNo)
+        editor.commit()
+    }
+
+    private fun readWordsFile(): kotlin.Array<String>{
+        //create local variables
+        val stringSplit = '&'
+        var tempString = ""
+
+        //reset variables
+        wordsFileArrayCounter = 0
+
+        //string splitting
+        for(i in textFileString.indices) {
+            if(textFileString[i] == stringSplit){
+
+                wordsFileArray[wordsFileArrayCounter] = tempString
+                wordsFileArrayCounter++
+                tempString = ""
+            } else {
+                tempString += textFileString[i]
+            }
+        }
+        wordsFileArray[wordsFileArrayCounter] = tempString
+        wordsFileArrayCounter++
+
+        return wordsFileArray
+    }
+
+    /*fun saveFile(){
+        //create local variables
+        var pref = PreferenceManager.getDefaultSharedPreferences(this)
+        var editor = pref.edit()
+        var pageNo = pref.getInt(savedPageNumber,1)
+        val pfd = contentResolver.openFileDescriptor(fileSelectedPath,"w")
+        val filestream = FileOutputStream(pfd.fileDescriptor)
+
+
+        //save updates to array
+        wordsFileArray[((pageNo+1)*2-1)] = editTextTerm.text.toString()
+        wordsFileArray[((pageNo+1)*2)] = editTextTerm.text.toString()
+
+        //convert to string
+        textFileString = ""
+        for (i in wordsFileArray.indices){
+            textFileString += wordsFileArray[i] + "&"
+        }
+/*
+        //write to file
+        grantUriPermission()
+        filestream.write(textFileString.toByteArray())
+        filestream.close()
+        pfd.close()*/
+    }*/
+
 }
